40dc76c6b8cf8e137b24a53d6e7416586c966835
LeonemZhang <764580636@qq.com>
2024-04-23 13:46:40 +0800

    pom

--- src/main/java/com/zzsn/common/GlobalExceptionHandler.java
+++ src/main/java/com/zzsn/common/GlobalExceptionHandler.java
@@ -4,10 +4,6 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
-/**
- * @author 张宗涵
- * @date 2024/4/20
- */
 @ControllerAdvice
 @Slf4j
 public class GlobalExceptionHandler {
